7731c8ec0c60ac06ce9a6e452ea9a61504367180
Moisès Macià <moises.macia@jobandtalent.com>
2022-10-31 12:38:41 +0100

    fix missing libraries for the latest version

--- aws-workspaces.sh
+++ aws-workspaces.sh
@@ -11,7 +11,9 @@ RUN apt-get update && apt-get install -y wget gnupg2
 RUN wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.com/ADB332E7.asc | apt-key add -
 RUN echo "deb [arch=amd64] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu bionic main" | tee /etc/apt/sources.list.d/amazon-workspaces-clients.list
 RUN apt-get update
-RUN apt-get install -y workspacesclient
+RUN apt-get install -y liblttng-ust0 \
+                       libusb-1.0-0 \
+                       workspacesclient
 CMD /opt/workspacesclient/workspacesclient
 EOF
 
